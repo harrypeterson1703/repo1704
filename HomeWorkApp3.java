fb31b8b71bb985aad8d10b4a377e8abd3ff4d626
harrypeterson1703 <103848887+harrypeterson1703@users.noreply.github.com>
2022-04-17 13:28:14 +0300

    Add files via upload

---
+++ HomeWorkApp3.java
@@ -0,0 +1,68 @@
+/**
+ * Java 1. Homework 3
+ *
+ * @author Igor
+ * @version 04.04.2022 edited
+ */
+ import java.util.Arrays;
+ 
+ class HomeWorkApp3 {
+    public static void main(String[] args) {
+        
+        invertArrayVal();
+        fillArray();
+        changeArray();
+        squareDiagonal();
+        arrayLenWithVal(5, 123);
+    }
+    
+    static void invertArrayVal() {
+        int[] arr = { 1, 0, 1 };
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1 - arr[i];
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    static void fillArray() {
+        int[] arrTwo = new int[100];
+        for (int i = 0; i < arrTwo.length; i++) {
+            arrTwo[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arrTwo));
+    }
+    
+    static void changeArray() {
+        int [] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+            arr[i] *= 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    static void squareDiagonal() {
+        int[][] arr = new int[9][9];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i][i] = 1;
+            arr[i][arr.length - i -1] = 1;
+            }
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+  
+  static void arrayLenWithVal(int len, int val) {
+        int[] arr = new int[len];
+        for(int i = 0; i < len; i++) {
+            arr[i] = val;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
